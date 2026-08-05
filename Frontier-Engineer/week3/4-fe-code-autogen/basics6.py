9a87b99173f972c3ba43bdd222181b401df8da22
Naresh Chaurasia <johndoe@example.com>
2026-08-05 13:23:02 +0530

    Macbook Pro, Checkin Timestamp::Wed Aug  5 13:23:02 IST 2026

--- Frontier-Engineer/week3/4-fe-code-autogen/basics6.py
+++ Frontier-Engineer/week3/4-fe-code-autogen/basics6.py
@@ -8,9 +8,6 @@ from autogen_agentchat.ui import Console
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_ext.tools.mcp import StdioServerParams, McpWorkbench
 
-os.environ[
-    "OPENAI_API_KEY"] = "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"
-
 
 async def main():
     filesystem_server_params = StdioServerParams( command="npx",
